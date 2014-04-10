769581bb5fb5d12f5207dd6df9c839692f551a1b
Sarah Sprague & Adam Willis <pair+spragglerock.whwebdev@devbootcamp.com>
2014-04-09 19:24:30 -0500

    new rails project

---
+++ cartogenesis/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cartogenesis::Application.config.secret_key_base = '36ab0fd561030a6af2d91d3d57d613757d08845e083a3eb1d34eaf3dff3f84cd47e1f4b630205aefa823ed86099f608f02840c739e9615576e9d67000bfdc5e6'
