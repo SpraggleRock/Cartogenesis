45209a503f3617d8ea5ea0985d2916564b9bc815
Sarah Sprague & Adam Willis <pair+spragglerock.whwebdev@devbootcamp.com>
2014-04-10 14:19:29 -0500

    removed artifacts of older file structure. With fire.

--- cartogenesis/spec/features/welcome_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-feature "the homepage"  do
-  scenario "the user can see the start game buton" do
-    visit root_path
-    expect(page).to have_button("Start Game")
-  end
-end
