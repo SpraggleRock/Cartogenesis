f92cccda16fd40dfe3f3dfa739d513839a7c7d27
Sarah Sprague & Adam Willis <pair+spragglerock.whwebdev@devbootcamp.com>
2014-04-10 10:55:26 -0500

    file naming, game controller spec

--- cartogenesis/spec/controllers/game_controller_spec.rb
+++ cartogenesis/spec/controllers/game_controller_spec.rb
@@ -1,5 +1,7 @@
-describe WelcomeController do
-  describes '#index' do
+require 'spec_helper'
+
+describe GameController do
+  describe '#new' do
     xit 'does some stuff eventually' do
       pending
     end
