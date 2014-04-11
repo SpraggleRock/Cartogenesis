ff7c240f8f9f516c877326d6bee998678dd843f8
Sarah Sprague & Adam Willis <pair+spragglerock.whwebdev@devbootcamp.com>
2014-04-11 16:08:48 -0500

    setup board

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,4 +1,15 @@
 class Board < ActiveRecord::Base
   has_many :tiles
   belongs_to :game
+
+  def setup_board(tile_radius)
+    (-board_size..board_size).to_a.each do |x|
+      (-board_size..board_size).to_a.each do |y|
+        (-board_size..board_size).to_a.each do |z|
+          self.tiles << Tile.new(radius: tile_radius,
+            a: x, b: y, c: z) if x + y + z == 0
+        end
+      end
+    end
+  end
 end
--- db/migrate/20140411174744_create_boards.rb
+++ db/migrate/20140411174744_create_boards.rb
@@ -2,7 +2,7 @@ class CreateBoards < ActiveRecord::Migration
   def change
     create_table :boards do |t|
       t.integer :radius
-      t.float :tile_radius
+      t.integer :board_size
 
       t.timestamps
     end
