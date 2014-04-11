91db4eb00973df1dbb1995bdc50cbfb09aba3995
Sarah Sprague & Adam Willis <pair+spragglerock.whwebdev@devbootcamp.com>
2014-04-11 07:51:16 -0500

    tile/tile adjacencies migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140410202604) do
+ActiveRecord::Schema.define(version: 20140411021738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "adjacencies", force: true do |t|
+    t.integer  "tile_id"
+    t.integer  "neighbor_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "games", force: true do |t|
     t.text     "game_svg"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tiles", force: true do |t|
+    t.integer  "game_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
