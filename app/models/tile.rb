c9540b896e0f191aa1826d01ddfaea02f7a8f57c
Sarah Sprague & Adam Willis <pair+spragglerock.whwebdev@devbootcamp.com>
2014-04-11 12:58:48 -0500

    removed 'adjacency' model, migration, associations'

--- app/models/tile.rb
+++ app/models/tile.rb
@@ -1,6 +1,4 @@
 class Tile < ActiveRecord::Base
-  has_many :adjacencies
-  has_many :neighbors, through: :adjacencies, source: :tile, foreign_key: "neighbor_id"
   belongs_to :game
   belongs_to :board
 
