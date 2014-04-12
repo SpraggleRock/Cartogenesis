11bc4d893541cbf7d02373911f6b0e0095de5163
Sarah Sprague & Adam Willis <pair+spragglerock.whwebdev@devbootcamp.com>
2014-04-12 11:58:30 -0500

    assign round points

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,7 @@
 class Game < ActiveRecord::Base
   has_many :tiles
   has_one :board
-  has_many :users
+  has_many :users through :users_games
+  has many :users_games
   has_many :rounds
 end
--- app/models/round.rb
+++ app/models/round.rb
@@ -1,9 +1,11 @@
 class Round < ActiveRecord::Base
   belongs_to :game
-  has_many :users through :game
+  has_many :users_games
   after_initialize :assign_round_points
 
   def assign_round_points
-
-
+    users_games do |usergames|
+      users_games.points += (rand(6) +rand(6) + 2)
+    end
+  end
 end
