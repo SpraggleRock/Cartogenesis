7c40bc3cdc2b140d61a0ded3cfa9595fc26d69ab
Sarah Sprague & Adam Willis <pair+spragglerock.whwebdev@devbootcamp.com>
2014-04-12 14:39:10 -0500

    moved round tracking to game

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,5 +3,14 @@ class Game < ActiveRecord::Base
   has_one :board
   has_many :players
   has_many :users, through: :players
-  has_many :rounds
+  has_many :ages
+
+
+  def assign_round_points
+    self.players.each do |player|
+      player.points = player.points + (rand(6) +rand(6) + 2)
+      player.save
+    end
+  end
+
 end
--- db/migrate/20140410202604_create_games.rb
+++ db/migrate/20140410202604_create_games.rb
@@ -1,7 +1,9 @@
 class CreateGames < ActiveRecord::Migration
   def change
     create_table :games do |t|
-
+      t.integer :round_counter, default: 0
+      t.integer :turn_counter, default: 0
+      t.integer :active_player
       t.timestamps
     end
   end
