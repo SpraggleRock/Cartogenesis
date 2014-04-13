3849a17431865ed8b47c5c5270c8abe5b6975234
Sarah Sprague & Adam Willis <pair+spragglerock.whwebdev@devbootcamp.com>
2014-04-13 10:32:00 -0500

    removed large swaths of commented out code

--- app/assets/javascripts/map_generator/map_generator.js
+++ app/assets/javascripts/map_generator/map_generator.js
@@ -73,41 +73,4 @@ $(".games.new").ready(function(){
       drawHexes(hex_data);
     });
   });
-
-
-
-  //Darws line and appends it to the svg
-  // var lineShow = svg.append("path")
-  //   .attr("d", lineFunction(hexagon).concat("Z"))
-  //   .attr("stroke", "black")
-  //   .attr("stroke-width", 1)
-  //   .attr("fill", "beige");
-
-  //Draws hexes and appends them to svg
 });
-
-
-
-  //  var fakeHexDatas = [ {'a':0,'b':0,'c':0}, {'a':1, 'b':0 , 'c':-1}, {'a':-1, 'b':1, 'c':0},
-  //   {'a':-1, 'b':0, 'c':1},{'a':0, 'b':1, 'c':-1}, {'a':0, 'b':-1, 'c':1},{'a':1, 'b':-1, 'c':0}, {'a':0,'b':-2,'c':2},
-  //   {'a':0,'b':2,'c':2},{'a':2,'b':-2,'c':0},{'a':1,'b':1,'c':-2},{'a':2,'b':0,'c':-2}]
-  // // var vertices = genLineData(genRandomPolarCoordinates(radius), [radius, radius]);
-
-  // hexagon = genHexData(genHexVertices(25), hexToCartesian(fakeHexDatas[0], 25))
-  // hexagon2 = genHexData(genHexVertices(25), hexToCartesian(fakeHexDatas[1], 25))
-  // hexagon3 = genHexData(genHexVertices(25), hexToCartesian(fakeHexDatas[2], 25))
-  // hexagon4 = genHexData(genHexVertices(25), hexToCartesian(fakeHexDatas[3], 25))
-  // hexagon5 = genHexData(genHexVertices(25), hexToCartesian(fakeHexDatas[4], 25))
-  // hexagon6 = genHexData(genHexVertices(25), hexToCartesian(fakeHexDatas[5], 25))
-  // hexagon7 = genHexData(genHexVertices(25), hexToCartesian(fakeHexDatas[6], 25))
-  // hexagon8 = genHexData(genHexVertices(25), hexToCartesian(fakeHexDatas[7], 25))
-  // hexagon9 = genHexData(genHexVertices(25), hexToCartesian(fakeHexDatas[8], 25))
-  // hexagon10 = genHexData(genHexVertices(25), hexToCartesian(fakeHexDatas[9], 25))
-  // hexagon11 = genHexData(genHexVertices(25), hexToCartesian(fakeHexDatas[10], 25))
-  // hexagon12 = genHexData(genHexVertices(25), hexToCartesian(fakeHexDatas[11], 25))
-
-  //  myGrid = [hexagon, hexagon2, hexagon3, hexagon4, hexagon5, hexagon6, hexagon7, hexagon8,
-  //  hexagon9,hexagon10,hexagon11,hexagon12]
-
-  //  console.log("here = " + myGrid[0])
-
