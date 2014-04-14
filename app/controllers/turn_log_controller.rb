70ddbf4966f915965d33533fd279eca05f196cdb
Sarah Sprague & Adam Willis <pair+spragglerock.whwebdev@devbootcamp.com>
2014-04-13 21:23:15 -0500

    create turn_logs in TurnLog#create

--- app/controllers/turn_log_controller.rb
+++ app/controllers/turn_log_controller.rb
@@ -1,5 +1,6 @@
 class TurnLogController < ApplicationController
 
   def create
+    TurnLog.create(documentation: params[:documentation], board_json: params[:board_json], game_id: params[:game_id])
   end
 end
