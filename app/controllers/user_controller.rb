f5ffab9c45bbcc7b8a88b8e54367686645fb4c03
Adam Willis <adam.willis@woodhouse.com>
2014-04-15 12:36:56 -0500

    implement error messages for invalid sign in

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -17,9 +17,9 @@ class UserController < ApplicationController
   end
 
   def create_session
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      sign_in user
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      sign_in @user
       redirect_to new_game_path
     else
       flash.now[:error] = 'Invalid email/password combination'
