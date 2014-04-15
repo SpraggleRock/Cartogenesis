8c172e9a57d2cf7f67355eaba921ef244c3d6a7d
Calder Wishne & Adam Willis <pair+CalderWishne.whwebdev@devbootcamp.com>
2014-04-15 08:14:38 -0500

    blew away dead code

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -2,9 +2,6 @@ function Chat() {
   this.dispatcher = new WebSocketRails('localhost:3000/websocket');
 };
 
-// Chat.prototype.init = function() {
-//   this.dispatcher.on_open = function(data) { console.log('Connection has been established', data)};
-// }
 Chat.prototype.setUpChat = function() {
   this.receiveAndDisplay();
   this.chatWindowOpenClose();
@@ -48,55 +45,8 @@ Chat.prototype.hookUpWebSocketsToSendMessage = function() {
   }.bind(this));
 }
 
-
-
-
-// console.log('in chat.js');
-// var dispatcher = new WebSocketRails('localhost:3000/websocket');
-
-// dispatcher.on_open = function(data) {
-//   console.log('Connection has been established', data);
-// }
-
-
-
-
 $(function(event){
   chat = new Chat();
   chat.setUpChat();
-//   // $('#open_chat').on('click', function(event){
-//   //   event.preventDefault();
-//   //   $('#chat_window').show();
-//   //   $('.chat_form').show();
-//   //   $(this).hide();
-//   //   $('#close_chat').show();
-//   // });
-//   // $('#close_chat').on('click', function(event){
-//   //   event.preventDefault();
-//   //   $('#chat_window').hide();
-//   //   $('.chat_form').hide();
-//   //   $(this).hide();
-//   //   $('#open_chat').show();
-//   // });
-//   // $('.chat_form').on('submit', function(event){
-//   //   event.preventDefault();
-//   //   var message = $('#chat_input').val();
-//   //   sendChatMessage(message);
-//   // });
 });
 
-
-// // function sendChatMessage(message) {
-// //   var message = { text: message };
-// //   dispatcher.trigger('chat_messages.broadcast', message );
-// //   console.log('sent message: ' + message.text);
-// // }
-
-// // function receiveAndDisplay() {
-// //   console.log('in receiveAndDisplay()')
-// //   dispatcher.bind('chat_messages.notification', function(message) {
-// //     console.log(message);
-
-// //     $('#chat_window').append("<p>"+message.text+"</p>");
-// //   });
-// // }
