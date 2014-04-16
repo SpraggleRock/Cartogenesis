e012d8a2f7c15e2a3efceb1ccf8958d6b80031e8
Calder Wishne & Adam Willis <pair+CalderWishne.whwebdev@devbootcamp.com>
2014-04-15 21:09:15 -0500

    user can't join the game more than once

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,8 +1,8 @@
 class PlayersController < ApplicationController
 
   def create
-    player = Player.find_or_create_by(player_params)
-    player.update(user_id: current_user.id)
+    player = Player.find_or_create_by(user_id: current_user.id)
+    player.update(player_params)
     player.update(game_id: params[:game_id])
     @game = player.game
     WebsocketRails[@game.slug.to_sym].trigger(:new_player, {player_name: player.name, username: current_user.username}.to_json)
