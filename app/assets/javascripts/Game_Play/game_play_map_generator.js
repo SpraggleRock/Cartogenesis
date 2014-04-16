8ed8b76796901c314ce6933403733404d3cfeb4a
Calder Wishne & Adam Willis <pair+CalderWishne.whwebdev@devbootcamp.com>
2014-04-16 11:05:03 -0500

    further small tweaks for game_play_map_generator.js

--- app/assets/javascripts/Game_Play/game_play_map_generator.js
+++ app/assets/javascripts/Game_Play/game_play_map_generator.js
@@ -1,5 +1,20 @@
 $(".games.play").ready(function(){
   var slugLength = 30;
+  var radius = 300;
+  var board;
+  var createdTiles = [];
+  var svg = d3.select(".play_game_svg_container").append("svg")
+   .attr("width", 2*radius)
+   .attr("height", 2*radius);
+  var g = d3.select("svg").append("g")
+    .attr("transform", "translate(" + [radius,radius] + ")");
+  var lineFunction = d3.svg.line()
+    .x(function(d){ return d.x;})
+    .y(function(d){ return d.y;})
+    .interpolate("linear");
+  var gameID = $('.games.play').attr("id");
+  var chronicleID = $('.chronicle_id').attr("id");
+
   socket = new Multiplayer(document.URL.slice(-slugLength));
 
   socket.update('end_turn', refresh);
@@ -7,10 +22,6 @@ $(".games.play").ready(function(){
   function refresh(data) {
     location.reload();
   }
-
-  var radius = 300;
-  var board;
-  var createdTiles = [];
 
   function Tile(radius, coordinates, terrain) {
     this.radius = radius;
@@ -25,19 +36,6 @@ $(".games.play").ready(function(){
     }
   }
 
-  var svg = d3.select(".play_game_svg_container").append("svg")
-   .attr("width", 2*radius)
-   .attr("height", 2*radius);
-
-  var g = d3.select("svg").append("g")
-    .attr("transform", "translate(" + [radius,radius] + ")");
-
-   //create line
-  var lineFunction = d3.svg.line()
-    .x(function(d){ return d.x;})
-    .y(function(d){ return d.y;})
-    .interpolate("linear");
-
   function format_coords(coord_string_array) {
     var coord_array = coord_string_array.map(function(coord_string){
       return coord_string.split(',');
@@ -47,26 +45,6 @@ $(".games.play").ready(function(){
     });
     return formatted_coord_array;
   }
-
-  function drawHexes(hexes) {
-    j = 0;
-    hexes.forEach(function(hex){
-      hexShow = g.append("path")
-        .attr("d", lineFunction(hex).concat("Z"))
-        .attr("stroke", "black")
-        .attr("stroke-width", 1)
-        .attr("terrain", board[j].terrain)
-        .attr("fill", getColor(board[j].terrain, allTools))
-        .attr("tile_id", board[j].id)
-        .attr("board_id", board[0].board_id)
-        .attr("coordinates", board[j].coordinates);
-
-        j++;
-    });
-  }
-
-  var gameID = $('.games.play').attr("id");
-  var chronicleID = $('.chronicle_id').attr("id");
 
   function drawHexes(hexes) {
     j = 0;
