041939fab91569531e5848e0f7e4ff3e29c66165
Calder Wishne & Adam Willis <pair+CalderWishne.whwebdev@devbootcamp.com>
2014-04-16 11:52:35 -0500

    players' names now append in game lobby

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,18 +1,19 @@
 class PlayersController < ApplicationController
 
   def create
-    player = Player.find_or_create_by_all(game_id: params[:game_id], user_id: current_user.id, name: params[:name])
-    player.update(name: params[:name])
+    player = Player.find_or_create_by_all(game_id: params[:game_id],
+      user_id: current_user.id, name: params[:player][:name])
+    player.update(name: params[:player][:name])
     @game = player.game
     WebsocketRails[@game.slug.to_sym].trigger(:new_player, {player_name: player.name, username: current_user.username}.to_json)
     puts
   end
 
 
-  private
-    def player_params
-      params.require(:player).permit(:name)
-    end
+  # private
+  #   def player_params
+  #     params.require(:player).permit(:name)
+  #   end
 
 
 end
