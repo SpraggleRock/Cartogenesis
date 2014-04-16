205c8020bfcf3288a1b7f9f2080204d54171eefd
Calder Wishne & Adam Willis <pair+CalderWishne.whwebdev@devbootcamp.com>
2014-04-16 09:12:36 -0500

    new player found by several fields before creating

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,9 +1,8 @@
 class PlayersController < ApplicationController
 
   def create
-    player = Player.find_or_create_by(user_id: current_user.id)
-    player.update(player_params)
-    player.update(game_id: params[:game_id])
+    player = Player.find_or_create_by_all(game_id: params[:game_id], user_id: current_user.id, name: params[:name])
+    player.update(name: params[:name])
     @game = player.game
     WebsocketRails[@game.slug.to_sym].trigger(:new_player, {player_name: player.name, username: current_user.username}.to_json)
     puts
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,17 @@
 class Player < ActiveRecord::Base
   belongs_to :user
   belongs_to :game
+
+  def self.find_or_create_by_all(params = {})
+    if self.find_by(user_id: params[:user_id])
+      player = self.find_by(user_id: params[:user_id])
+      if (player.game_id == params[:game_id])
+        return player
+      else
+        self.create(params)
+      end
+    else
+      self.create(params)
+    end
+  end
 end
