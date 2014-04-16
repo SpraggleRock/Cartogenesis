077ec4f97616f997dc06fb68d39cc4669f488151
Calder Wishne & Adam Willis <pair+CalderWishne.whwebdev@devbootcamp.com>
2014-04-15 21:31:53 -0500

    fix error where history logs with no next log freak out

--- app/controllers/turn_logs_controller.rb
+++ app/controllers/turn_logs_controller.rb
@@ -19,7 +19,9 @@ class TurnLogsController < ApplicationController
     logs = @chronicle.turn_logs.order(id: :asc)
     @turn_log = @chronicle.turn_logs.find(params[:id])
     next_index = logs.find_index{|log| log.id == @turn_log.id} + 1
-    @next = logs[next_index].id
+    if logs[next_index]
+      @next = logs[next_index].id
+    end
   end
 
   def return_json
