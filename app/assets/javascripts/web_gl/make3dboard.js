1d4987bc6f3a0c91c499d93e1f3e977748a490af
chrisscherer <chrisscherer90@gmail.com>
2014-04-15 20:59:07 -0500

    renders 3d terrain

--- app/assets/javascripts/web_gl/make3dboard.js
+++ app/assets/javascripts/web_gl/make3dboard.js
@@ -3,13 +3,18 @@ $( document ).ready(function() {
     var board3d = [];
 
 
-    var grabBoard = $.getJSON( '/board/'+ 10, function(data){
+    var grabBoard = $.getJSON( '/board/'+ 18, function(data){
       data.forEach(function(datum){
         board3d.push(datum['terrain'])
       })
     });
 
   grabBoard.complete(function(){
+
+    function randomIntFromInterval(min,max)
+    {
+      return Math.floor(Math.random()*(max-min+1)+min);
+    }
 
     var stats = new Stats();
     stats.setMode(1); // 0: fps, 1: ms
@@ -37,50 +42,50 @@ $( document ).ready(function() {
       function makeHex(vertices, radius, terrain){
         var side1 = new THREE.Geometry();
         var side2 = new THREE.Geometry();
+
+
         
         //cache materials instead of making them every time
         if(terrain == 'ocean'){
           var material = new THREE.MeshLambertMaterial( { color: '#0000ff' } );
-          z = -1
-          console.log('ocean')
+          z = 0
         }
         else if(terrain == 'grassland'){
           var material = new THREE.MeshLambertMaterial( { color: '#99FF33' } );
-          z = 5
-          console.log('grassland')
+          z = 10
         }
         else if(terrain == 'desert'){
           var material = new THREE.MeshLambertMaterial( { color: '#DBB84D' } );
-          z = 5
+          z = 12
         }
         else if(terrain == 'tundra'){
           var material = new THREE.MeshLambertMaterial( { color: '#B8E6E6' } );
-          z = 3
+          z = 10
         }
         else if(terrain == 'forest'){
           var material = new THREE.MeshLambertMaterial( { color: '#006600' } );
-          z = 8
+          z = 10
         }
         else if(terrain == 'mountain'){
           var material = new THREE.MeshLambertMaterial( { color: '#999999' } );
-          z = 30
+          z = 12
         }
+
+        vertices.forEach(function(vert){
+          point = new THREE.Vector3(vert.x, vert.y, z);
+          point2 = new THREE.Vector3(vert.x, vert.y, 0 - 20);
+          side1.vertices.push( point );
+          side2.vertices.push( point2);
+        });
+
 
 
         material.side = THREE.DoubleSide;
 
-
-        vertices.forEach(function(vert){
-          point = new THREE.Vector3(vert.x, vert.y, z);
-          //point2 = new THREE.Vector3(vert.x, vert.y, 0 - 20);
-          side1.vertices.push( point );
-          //side2.vertices.push( point2);
-        });
-
         var i = 1;
         vertices.forEach(function(vert){
           side1.faces.push( new THREE.Face3(0, i, i+1))
-          //side2.faces.push( new THREE.Face3(0, i, i+1))
+          side2.faces.push( new THREE.Face3(0, i, i+1))
           if(i == 5){
             i = 1;
           }
@@ -89,21 +94,103 @@ $( document ).ready(function() {
           }
         })
 
-        //THREE.GeometryUtils.merge(side1, side2)
+        if(terrain == 'mountain'){
+          side1.vertices.push( new THREE.Vector3((side1.vertices[0].x + side1.vertices[3].x)/2, side1.vertices[0].y, 50) )
+          for(i=1; i < 7;i++){
+            side1.faces.push( new THREE.Face3(i,i+1,7))
+          }
+          side1.faces.push( new THREE.Face3(6,1,7))
+        }
 
-        //facesToCreate1 = [[1,2,8],[2,3,9],[3,4,10],[4,5,11],[5,6,12],[6,7,13]]
-        //facesToCreate2 = [[2,8,9],[3,9,10],[4,10,11],[5,11,12],[6,12,13],[7,13,1]]
+        THREE.GeometryUtils.merge(side1, side2)
 
-        //for(i=0;i<6;i++){
-          //side1.faces.push( new THREE.Face3(facesToCreate1[i][0],facesToCreate1[i][1],facesToCreate1[i][2]))
-          //side1.faces.push( new THREE.Face3(facesToCreate2[i][0],facesToCreate2[i][1],facesToCreate2[i][2]))
-        //}
+        facesToCreate1 = [[1,2,8],[2,3,9],[3,4,10],[4,5,11],[5,6,12],[6,1,13]]
+        facesToCreate2 = [[8,13,1],[13,12,6],[12,11,5],[11,10,4],[10,9,3],[9,8,2]]
+
+        for(i=0;i<6;i++){
+          side1.faces.push( new THREE.Face3(facesToCreate1[i][0],facesToCreate1[i][1],facesToCreate1[i][2]))
+          side1.faces.push( new THREE.Face3(facesToCreate2[i][0],facesToCreate2[i][1],facesToCreate2[i][2]))
+        }
+
+        if(terrain == 'desert'){
+          randomLocations = []
+          var dunes = []
+          for(i=0;i<2;i++){
+            dune = new THREE.CylinderGeometry( 2, 3, 20, 3 );
+            dune.applyMatrix(new THREE.Matrix4().makeRotationZ(randomIntFromInterval(0,1.57)))
+            dune.applyMatrix(new THREE.Matrix4().makeTranslation(randomIntFromInterval(side1.vertices[1].x, side1.vertices[4].x) ,randomIntFromInterval(side1.vertices[1].y, side1.vertices[5].y - 2), side1.vertices[0].z))
+            THREE.GeometryUtils.merge(side1, dune)
+          }
+        }
+
+        if(terrain == 'forest'){
+          trunks = []
+          treeTops = []
+          randomLocations = []
+          for(i=0;i<30;i++){
+            trunk = new THREE.CylinderGeometry(1,1,10,10,10,false)
+            trunk.applyMatrix(new THREE.Matrix4().makeRotationX(1.57))
+            randomLocations.push([randomIntFromInterval(side1.vertices[1].x, side1.vertices[4].x), randomIntFromInterval(side1.vertices[1].y, side1.vertices[5].y)])
+            trunk.applyMatrix(new THREE.Matrix4().makeTranslation(randomLocations[i][0], randomLocations[i][1], side1.vertices[0].z + 2))
+            trunks.push(trunk)
+          }
+
+          for(i=0; i<30; i++){
+            hold = new THREE.SphereGeometry(3,10,10)
+            hold.applyMatrix(new THREE.Matrix4().makeTranslation(randomLocations[i][0], randomLocations[i][1], side1.vertices[0].z + 7))//(side1.vertices[0].x + side1.vertices[3].x)/2, side1.vertices[0].y, side1.vertices[0].z + 10)
+            treeTops.push(hold)
+          }
+
+
+          trunks.forEach(function(trunk){
+            THREE.GeometryUtils.merge(side1, trunk)
+          })
+          treeTops.forEach(function(top){
+            THREE.GeometryUtils.merge(side1, top)
+          })
+        }
+
+        if (terrain == 'tundra'){
+          var particleCount = 50,
+          particles = new THREE.Geometry(),
+          pMaterial = new THREE.ParticleBasicMaterial({
+            color: 0xffffff,
+            size: 2
+          });
+
+      // now create the individual particles
+      for (var p = 0; p < particleCount; p++) {
+        
+        var pX = randomIntFromInterval(side1.vertices[0].x, side1.vertices[3].x),
+            pY = randomIntFromInterval(side1.vertices[1].y, side1.vertices[5].y),
+            pZ = 20,
+            particle = new THREE.Vector4(pX, pY, pZ, 0)
+
+        // add it to the geometry
+        particles.vertices.push(particle);
+      }
+
+      // create the particle system
+      var particleSystem = new THREE.ParticleSystem(
+          particles,
+          pMaterial);
+
+        particleSystem.name = 'particleSys'
+
+        scene.add( particleSystem )
+        }
 
         side1.computeCentroids();
         side1.computeFaceNormals();
         side1.computeVertexNormals();
 
         var hex1 = new THREE.Mesh( side1, material );
+        if(terrain == 'ocean'){
+          hex1.ocean_delta = 5
+        }
+        if(terrain == 'tundra'){
+          hex1.tundra_delta = 5
+        }
 
         return hex1;
       }
@@ -115,6 +202,44 @@ $( document ).ready(function() {
         k++;
       })
       return boardHexes
+    }
+
+    var y = 0
+    function makeSomeWaves(){
+      boardHexes.forEach(function(hex){
+        var deltaZ = 1
+        if(hex.ocean_delta && y % 50 == 0){
+          if(hex.position.z > 5 || hex.position.z < -5 ){
+            deltaZ = -deltaZ
+          }
+          hex.applyMatrix(new THREE.Matrix4().makeTranslation(0, 0, randomIntFromInterval(deltaZ, 0)))
+        }
+      })
+      y++
+    }
+
+    g = 0
+    function makeItSnow(){
+      scene.__objectsAdded.forEach(function(object){
+        console.log(object)
+        if(object.name == 'particleSys' && g % 10 == 0){
+          console.log(object.position)
+        //   object.geometry.vertices.forEach(function(particle){
+        //     console.log(object)
+        //       if (particle.y < 0) {
+        //         particle.y = 20;
+        //         particle.velocity.y = 0;
+        //       }
+        //       particle.velocity.y -= Math.random() * .1;
+
+        //       particle.position.addSelf(particle.velocity);
+
+        //       particleSystem.geometry.__dirtyVertices = true;
+        //   })
+        // }
+      }
+      })
+      g++
     }
     
     makeBoard(hex_data);
@@ -129,12 +254,14 @@ $( document ).ready(function() {
 
     controls = new THREE.OrbitControls(camera);
 
-    pointLight.position.z = 150
+    pointLight.position.z = 400
     camera.position.z = 5;
 
     function render() {
       stats.begin();
       controls.update();
+      makeSomeWaves();
+      makeItSnow();
       requestAnimationFrame(render);
       renderer.render(scene, camera);
       stats.end();
